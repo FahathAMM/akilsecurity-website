df3fa7fa6a4bd4f52d06c251def7eab7e470051f
fahathAMM <fahathammex90@gmail.com>
2025-12-20 02:20:04 +0400

    Create site.php

---
+++ config/site.php
@@ -0,0 +1,386 @@
+<?php
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Home Page
+    |--------------------------------------------------------------------------
+    */
+    'home' => [
+        'banner' => [
+            'title-company' => 'Akil Security & Alarm System',
+            'title'         => 'IoT-powered security, anytime, anywhere',
+            'description'   => 'Enhance workplace efficiency and safety with IoT-powered time attendance, security, and alarm solutions designed to protect people, assets seamlessly..',
+        ],
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | About Page
+    |--------------------------------------------------------------------------
+    */
+
+    'about' => [
+        'main-title' => 'ABOUT US',
+        'title'         => ['Your trusted in our', 'IOT & alarm security solutions'],
+        'description'   => 'We provide reliable, cutting-edge IOT & alarm security solutions to protect your digital assets, ensuring safety and peace of mind.',
+        'points'         => [
+            'Threat Detection and Monitoring',
+            'Access Control Management',
+            'Incident Response and Recovery',
+            'Security Awareness Training'
+        ],
+        'mobile' => '+971 52 482 0440',
+        'support-description' => 'Real-time support for all workplace safety and attendance needs, including IoT monitoring, security alerts, and system guidance',
+
+        'our_approach' => [
+            'section_title' => 'our approach',
+            'title' => [
+                'Strengthening security,',
+                'your future',
+            ],
+            'image' => 'site/images/our-approach-image.jpg',
+
+            'items' => [
+                [
+                    'icon' => 'site/images/icon-our-mission.svg',
+                    'title' => 'our mission',
+                    'description' => 'To empower businesses with robust cybersecurity solutions, ensuring data integrity, privacy & resilience against threats.',
+                ],
+                [
+                    'icon' => 'site/images/icon-our-vision.svg',
+                    'title' => 'our vision',
+                    'description' => 'To be a global leader in cybersecurity innovation and trusted digital protection.',
+                ],
+                [
+                    'icon' => 'site/images/icon-our-value.svg',
+                    'title' => 'our value',
+                    'description' => 'Integrity, innovation, customer trust, and continuous improvement.',
+                ],
+            ],
+        ],
+
+        'what_we_do' => [
+            'section_title' => 'what we do',
+            'title' => [
+                'Protecting, preventing,',
+                'securing your future',
+            ],
+            'description' => 'We provide comprehensive cybersecurity services to safeguard your digital assets, prevent potential threats, and ensure a secure environment.',
+
+            'experience' => [
+                'years' => 25,
+                'text' => 'Years of experience',
+            ],
+
+            'images' => [
+                'image_1' => 'site/images/what-we-do-img-1.jpg',
+                'image_2' => 'site/images/what-we-do-img-2.jpg',
+            ],
+
+            'points' => [
+                'Proactive Threat Detection and Incident Response',
+                'Advanced Network Security and Intrusion Prevention',
+                'Data Encryption and Secure Information Management',
+            ],
+
+            'items' => [
+                [
+                    'icon' => 'site/images/icon-what-we-item-1.svg',
+                    'title' => 'network and firewall security',
+                ],
+                [
+                    'icon' => 'site/images/icon-what-we-item-2.svg',
+                    'title' => 'data protection and encryption',
+                ],
+            ],
+        ],
+
+        'why_choose_us' => [
+            'section_title' => 'why choose us',
+            'title' => 'Reliable solutions for cybersecurity excellence',
+            'image' => 'site/images/why-choose-image.jpg',
+            'items' => [
+                [
+                    'icon' => 'site/images/icon-why-choose-1.svg',
+                    'title' => 'expertise and experience',
+                    'description' => 'A team of seasoned cybersecurity professionals with extensive industry knowledge.',
+                ],
+                [
+                    'icon' => 'site/images/icon-why-choose-2.svg',
+                    'title' => 'proactive security approach',
+                    'description' => 'Focused on preventing threats before they impact your system, not just reacting after the fact.',
+                ],
+                [
+                    'icon' => 'site/images/icon-why-choose-3.svg',
+                    'title' => 'tailored training programs',
+                    'description' => 'Educating your team on security best practices to reduce human error and enhance vigilance.',
+                ],
+            ],
+
+            'counters' => [
+                [
+                    'icon' => 'site/images/icon-why-choose-counter-1.svg',
+                    'value' => 25,
+                    'suffix' => '+',
+                    'label' => 'Years Experience',
+                ],
+                [
+                    'icon' => 'site/images/icon-why-choose-counter-2.svg',
+                    'value' => 15,
+                    'suffix' => 'K',
+                    'label' => 'project complete',
+                ],
+                [
+                    'icon' => 'site/images/icon-why-choose-counter-3.svg',
+                    'value' => 16,
+                    'suffix' => 'K',
+                    'label' => 'customer happy',
+                ],
+                [
+                    'icon' => 'site/images/icon-why-choose-counter-4.svg',
+                    'value' => 120,
+                    'suffix' => '',
+                    'label' => 'winning awards',
+                ],
+            ],
+        ],
+
+        'how_it_work' => [
+            'section_title' => 'how it work',
+            'title' => [
+                'Streamlined process, cyber',
+                'protection security',
+            ],
+
+            'steps' => [
+                [
+                    'step' => '01',
+                    'title' => 'assessment & planning',
+                    'description' => 'We start by understand your unique needs potential risks Through detailed assessment we identify vulnerabilities and develop.',
+                ],
+                [
+                    'step' => '02',
+                    'title' => 'implementation',
+                    'description' => 'We start by understand your unique needs potential risks Through detailed assessment we identify vulnerabilities and develop.',
+                ],
+                [
+                    'step' => '03',
+                    'title' => 'ongoing support & maintenance',
+                    'description' => 'We start by understand your unique needs potential risks Through detailed assessment we identify vulnerabilities and develop.',
+                ],
+            ],
+
+            'images' => [
+                'image_1' => 'site/images/how-it-work-image-1.jpg',
+                'image_2' => 'site/images/how-it-work-image-2.jpg',
+            ],
+        ],
+
+        'our_team' => [
+            'section_title' => 'Expert team',
+            'title' => 'Our expert team member',
+            'members' => [
+                [
+                    'name' => 'michael johnson',
+                    'role' => 'security officer',
+                    'image' => 'site/images/team-1.jpg',
+                    'link' => 'team-single.html',
+                    'socials' => [
+                        'twitter' => '#',
+                        'facebook' => '#',
+                        'instagram' => '#',
+                    ],
+                ],
+                [
+                    'name' => 'sophia carter',
+                    'role' => 'cybersecurity analyst',
+                    'image' => 'site/images/team-2.jpg',
+                    'link' => 'team-single.html',
+                    'socials' => [
+                        'twitter' => '#',
+                        'facebook' => '#',
+                        'instagram' => '#',
+                    ],
+                ],
+                [
+                    'name' => 'michael brown',
+                    'role' => 'incident response',
+                    'image' => 'site/images/team-3.jpg',
+                    'link' => 'team-single.html',
+                    'socials' => [
+                        'twitter' => '#',
+                        'facebook' => '#',
+                        'instagram' => '#',
+                    ],
+                ],
+                [
+                    'name' => 'isabella moore',
+                    'role' => 'security awareness',
+                    'image' => 'site/images/team-4.jpg',
+                    'link' => 'team-single.html',
+                    'socials' => [
+                        'twitter' => '#',
+                        'facebook' => '#',
+                        'instagram' => '#',
+                    ],
+                ],
+            ],
+        ],
+
+        'our_team' => [
+            'section_title' => 'Expert team',
+            'title' => 'Our expert team member',
+            'members' => [
+                [
+                    'name' => 'michael johnson',
+                    'role' => 'security officer',
+                    'image' => 'site/images/team-1.jpg',
+                    'link' => 'team-single.html',
+                    'socials' => [
+                        'twitter' => '#',
+                        'facebook' => '#',
+                        'instagram' => '#',
+                    ],
+                ],
+                [
+                    'name' => 'sophia carter',
+                    'role' => 'cybersecurity analyst',
+                    'image' => 'site/images/team-2.jpg',
+                    'link' => 'team-single.html',
+                    'socials' => [
+                        'twitter' => '#',
+                        'facebook' => '#',
+                        'instagram' => '#',
+                    ],
+                ],
+                [
+                    'name' => 'michael brown',
+                    'role' => 'incident response',
+                    'image' => 'site/images/team-3.jpg',
+                    'link' => 'team-single.html',
+                    'socials' => [
+                        'twitter' => '#',
+                        'facebook' => '#',
+                        'instagram' => '#',
+                    ],
+                ],
+                [
+                    'name' => 'isabella moore',
+                    'role' => 'security awareness',
+                    'image' => 'site/images/team-4.jpg',
+                    'link' => 'team-single.html',
+                    'socials' => [
+                        'twitter' => '#',
+                        'facebook' => '#',
+                        'instagram' => '#',
+                    ],
+                ],
+            ],
+        ],
+
+        'our_testimonials' => [
+            'section_title' => 'testimonials',
+            'title' => ['Our clients', 'are saying'],
+
+            'video' => [
+                'url' => 'https://www.youtube.com/watch?v=Y-x0efG1seA',
+                'label' => 'Watch video',
+            ],
+
+            'slides' => [
+                [
+                    'company_logo' => 'site/images/company-logo.svg',
+                    'quote_icon' => 'site/images/testimonial-quote.svg',
+                    'content' => [
+                        "This cybersecurity service has been a game-changer for my business. The Basic Plan keeps my data secure, and I no longer worry about online threats. The setup was simple, and the team provided excellent support. Highly recommend!",
+                        "I love how easy it is to use this service. It's affordable and gives me the confidence to work online without fear of breaches. A must-have for anyone who values their online safety!"
+                    ],
+                    'author' => [
+                        'name' => 'Alex J.',
+                        'role' => 'IT manager',
+                        'image' => 'site/images/author-1.jpg',
+                    ],
+                ],
+                [
+                    'company_logo' => 'site/images/company-logo.svg',
+                    'quote_icon' => 'site/images/testimonial-quote.svg',
+                    'content' => [
+                        "This cybersecurity service has been a game-changer for my business. The Basic Plan keeps my data secure, and I no longer worry about online threats. The setup was simple, and the team provided excellent support. Highly recommend!",
+                        "I love how easy it is to use this service. It's affordable and gives me the confidence to work online without fear of breaches. A must-have for anyone who values their online safety!"
+                    ],
+                    'author' => [
+                        'name' => 'Sarah Mitchell',
+                        'role' => 'Cloud manager',
+                        'image' => 'site/images/author-2.jpg',
+                    ],
+                ],
+            ],
+
+            'contact' => [
+                'phone' => '+91-123 456 789',
+                'contact_page' => 'contact.html',
+            ],
+
+        ],
+
+        'our_benefits' => [
+            'section_title' => 'our benefits',
+            'title' => 'Reliable solutions for cybersecurity excellence',
+            'description' => 'Our comprehensive cybersecurity solutions provide robust protection, minimizing risks and giving you peace of mind knowing your data and systems are secure. We empower you with the tools and expertise to navigate today\'s.',
+
+            'images' => [
+                'image_1' => 'site/images/benefit-image-1.jpg',
+                'image_2' => 'site/images/benefit-image-2.jpg',
+                'circle_image' => 'site/images/contact-circle-image.svg',
+            ],
+
+            'body_items' => [
+                [
+                    'icon' => 'site/images/icon-benefit-body-1.svg',
+                    'title' => 'proactive threat protection',
+                ],
+                [
+                    'icon' => 'site/images/icon-benefit-body-2.svg',
+                    'title' => '24/7 monitoring and help',
+                ],
+            ],
+
+            'footer_list' => [
+                'Enhanced Business Continuity',
+                'Scalable Solutions',
+                'Peace of Mind',
+            ],
+
+            'footer_button' => [
+                'label' => 'get started',
+                'link' => 'contact.html',
+            ],
+
+        ],
+
+        'faq' => [
+            'section_title' => 'faq',
+            'title' => ['Cybersecurity questions', 'answered clearly'],
+            'image' => 'site/images/faq-image.jpg',
+            'items' => [
+                [
+                    'question' => 'What is cybersecurity?',
+                    'answer' => 'Cybersecurity is critical for safeguarding sensitive information, ensuring business continuity, and preventing financial and reputational loss due to cyberattacks.'
+                ],
+                [
+                    'question' => 'Why is cybersecurity important?',
+                    'answer' => 'Cybersecurity is critical for safeguarding sensitive information, ensuring business continuity, and preventing financial and reputational loss due to cyberattacks.'
+                ],
+                [
+                    'question' => 'What are the most common cyber threats?',
+                    'answer' => 'Cybersecurity is critical for safeguarding sensitive information, ensuring business continuity, and preventing financial and reputational loss due to cyberattacks.'
+                ],
+                [
+                    'question' => 'How can I protect my business from cyber threats?',
+                    'answer' => 'Cybersecurity is critical for safeguarding sensitive information, ensuring business continuity, and preventing financial and reputational loss due to cyberattacks.'
+                ],
+            ],
+        ],
+    ],
+];
