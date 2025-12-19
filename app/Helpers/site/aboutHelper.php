276f8307a53444af7051609e33602ba9f2785fd9
fahathAMM <fahathammex90@gmail.com>
2025-12-20 02:19:45 +0400

    Update aboutHelper.php

--- app/Helpers/site/aboutHelper.php
+++ app/Helpers/site/aboutHelper.php
@@ -1,15 +1,13 @@
 <?php
 
 if (!function_exists('about_main_title')) {
-
     function about_main_title()
     {
-        return config('about.main-title');
+        return config('site.about.main-title');
     }
 }
 
 if (!function_exists('home_banner_description')) {
-
     function home_banner_description()
     {
         return config('home.banner.description');
@@ -17,25 +15,323 @@ if (!function_exists('home_banner_description')) {
 }
 
 if (!function_exists('about_title')) {
-
     function about_title()
     {
-        return config('about.title');
+        return config('site.about.title');
+    }
+}
+
+if (!function_exists('about_mobile')) {
+    function about_mobile()
+    {
+        return config('site.about.mobile');
     }
 }
 
 if (!function_exists('about_points')) {
-
     function about_points()
     {
-        return config('about.points');
+        return config('site.about.points');
     }
 }
 
 if (!function_exists('about_support_description')) {
-
     function about_support_description()
     {
-        return config('about.support-description');
+        return config('site.about.support-description');
     }
 }
+
+// howItWorkHelper
+if (!function_exists('how_it_work_section_title')) {
+    function how_it_work_section_title()
+    {
+        return config('site.about.how_it_work.section_title');
+    }
+}
+
+if (!function_exists('how_it_work_title')) {
+    function how_it_work_title()
+    {
+        return config('site.about.how_it_work.title');
+    }
+}
+
+if (!function_exists('how_it_work_steps')) {
+    function how_it_work_steps()
+    {
+        return config('site.about.how_it_work.steps');
+    }
+}
+
+if (!function_exists('how_it_work_images')) {
+    function how_it_work_images()
+    {
+        return config('site.about.how_it_work.images');
+    }
+}
+
+// howItWorkHelper
+
+// ourApproachHelper
+if (!function_exists('our_approach_section_title')) {
+    function our_approach_section_title()
+    {
+        return config('site.about.our_approach.section_title');
+    }
+}
+
+if (!function_exists('our_approach_title')) {
+    function our_approach_title()
+    {
+        return config('site.about.our_approach.title');
+    }
+}
+
+if (!function_exists('our_approach_items')) {
+    function our_approach_items()
+    {
+        return config('site.about.our_approach.items');
+    }
+}
+
+if (!function_exists('our_approach_image')) {
+    function our_approach_image()
+    {
+        return config('site.about.our_approach.image');
+    }
+}
+// ourApproachHelper
+
+
+// whatWeDoHelper
+if (!function_exists('what_we_do_section_title')) {
+    function what_we_do_section_title()
+    {
+        return config('site.about.what_we_do.section_title');
+    }
+}
+
+if (!function_exists('what_we_do_title')) {
+    function what_we_do_title()
+    {
+        return config('site.about.what_we_do.title');
+    }
+}
+
+if (!function_exists('what_we_do_description')) {
+    function what_we_do_description()
+    {
+        return config('site.about.what_we_do.description');
+    }
+}
+
+if (!function_exists('what_we_do_experience')) {
+    function what_we_do_experience()
+    {
+        return config('site.about.what_we_do.experience');
+    }
+}
+
+if (!function_exists('what_we_do_images')) {
+    function what_we_do_images()
+    {
+        return config('site.about.what_we_do.images');
+    }
+}
+
+if (!function_exists('what_we_do_points')) {
+    function what_we_do_points()
+    {
+        return config('site.about.what_we_do.points');
+    }
+}
+
+if (!function_exists('what_we_do_items')) {
+    function what_we_do_items()
+    {
+        return config('site.about.what_we_do.items');
+    }
+}
+// whatWeDoHelper
+
+
+// whyChooseUsHelper
+if (!function_exists('why_choose_us_section_title')) {
+    function why_choose_us_section_title()
+    {
+        return config('site.about.why_choose_us.section_title');
+    }
+}
+
+if (!function_exists('why_choose_us_title')) {
+    function why_choose_us_title()
+    {
+        return config('site.about.why_choose_us.title');
+    }
+}
+
+if (!function_exists('why_choose_us_image')) {
+    function why_choose_us_image()
+    {
+        return config('site.about.why_choose_us.image');
+    }
+}
+
+if (!function_exists('why_choose_us_items')) {
+    function why_choose_us_items()
+    {
+        return config('site.about.why_choose_us.items');
+    }
+}
+
+if (!function_exists('why_choose_us_counters')) {
+    function why_choose_us_counters()
+    {
+        return config('site.about.why_choose_us.counters');
+    }
+}
+
+// whyChooseUsHelper
+
+
+// ourTeam
+if (!function_exists('our_team_section_title')) {
+    function our_team_section_title()
+    {
+        return config('site.about.our_team.section_title');
+    }
+}
+
+if (!function_exists('our_team_title')) {
+    function our_team_title()
+    {
+        return config('site.about.our_team.title');
+    }
+}
+
+if (!function_exists('our_team_members')) {
+    function our_team_members()
+    {
+        return config('site.about.our_team.members');
+    }
+}
+// ourTeam
+
+// testimonials
+if (!function_exists('testimonials_section_title')) {
+    function testimonials_section_title()
+    {
+        return config('site.about.our_testimonials.section_title');
+    }
+}
+
+if (!function_exists('testimonials_title')) {
+    function testimonials_title()
+    {
+        return config('site.about.our_testimonials.title');
+    }
+}
+
+if (!function_exists('testimonials_video')) {
+    function testimonials_video()
+    {
+        return config('site.about.our_testimonials.video');
+    }
+}
+
+if (!function_exists('testimonials_slides')) {
+    function testimonials_slides()
+    {
+        return config('site.about.our_testimonials.slides');
+    }
+}
+
+if (!function_exists('testimonials_contact')) {
+    function testimonials_contact()
+    {
+        return config('site.about.our_testimonials.contact');
+    }
+}
+// testimonials
+
+// our_benefits
+if (!function_exists('benefits_section_title')) {
+    function benefits_section_title()
+    {
+        return config('site.about.our_benefits.section_title');
+    }
+}
+
+if (!function_exists('benefits_title')) {
+    function benefits_title()
+    {
+        return config('site.about.our_benefits.title');
+    }
+}
+
+if (!function_exists('benefits_description')) {
+    function benefits_description()
+    {
+        return config('site.about.our_benefits.description');
+    }
+}
+
+if (!function_exists('benefits_images')) {
+    function benefits_images()
+    {
+        return config('site.about.our_benefits.images');
+    }
+}
+
+if (!function_exists('benefits_body_items')) {
+    function benefits_body_items()
+    {
+        return config('site.about.our_benefits.body_items');
+    }
+}
+
+if (!function_exists('benefits_footer_list')) {
+    function benefits_footer_list()
+    {
+        return config('site.about.our_benefits.footer_list');
+    }
+}
+
+if (!function_exists('benefits_footer_button')) {
+    function benefits_footer_button()
+    {
+        return config('site.about.our_benefits.footer_button');
+    }
+}
+// our_benefits
+
+// faq
+if (!function_exists('faq_section_title')) {
+    function faq_section_title()
+    {
+        return config('site.about.faq.section_title');
+    }
+}
+
+if (!function_exists('faq_title')) {
+    function faq_title()
+    {
+        return config('site.about.faq.title');
+    }
+}
+
+if (!function_exists('faq_items')) {
+    function faq_items()
+    {
+        return config('site.about.faq.items');
+    }
+}
+
+if (!function_exists('faq_image')) {
+    function faq_image()
+    {
+        return config('site.about.faq.image');
+    }
+}
+// faq
